8f70a55bc1e2ad19e15f5ea1e5d23595c7665be0
Pr0m3th3usEx <pr0m3th3us@kron.os>
2020-04-30 23:05:11 +0200

    [ADD] reshape_function

--- color.py
+++ color.py
@@ -26,22 +26,41 @@ env = gym.make("Duckietown-udem1-v0")
 env.reset()
 env.render()
 
+"""
+    reshape_area : Resize the rect to fit the size of the image
+    
+    Arguments : img_w, img_h : size of the image, Rect()
+    Return : Rect
+"""
+def reshape_area(img_w, img_h, area):
+    if area.x > img_w or area.y > img_h:
+        raise ValueError
+    if area.x + area.w > img_w:
+        area.w = area.x + area.w - img_w
+    if area.y + area.h > img_h:
+        area.h = area.y + area.h - img_h
+    print(area)
+    return area
+
 
 """
-    get_major_color : From a Pixel array, 
-    return the dominant color of a specific area
+    get_major_color : From a Pixel array, return the dominant color of a specific area
+    
     Arguments : img_array : np.array, area: Rect
     Return : (R, G, B): tuple , covered_area: float
 """
-def get_major_color(img_array:np.array, area):
+def get_major_color(img_array:np.ndarray, area):
     max_w, max_h, nb_channels = img_array.shape
-    dom_r = 0
-    dom_g = 0
-    dom_b = 0
-    divider = area.w * area.h
+    dom_r, dom_g, dom_b = 0, 0, 0
+    div = area.w * area.h
+
     for off_y in range(area.w):
         for off_x in range(area.h):
-            print(img_array[area.x + off_x, area.y + off_y])
+            pixel = img_array[area.y + off_y, area.x + off_x]
+            dom_r += pixel[0]
+            dom_g += pixel[1]
+            dom_b += pixel[2]
+    return dom_r / div, dom_g / div, dom_b / div
 
 @env.unwrapped.window.event
 def on_key_press(symbol, modifier):
@@ -55,9 +74,10 @@ env.unwrapped.window.push_handlers(key_handler)
 action = np.array([0.3, 0.00])
 obs, reward, done, info = env.step(action)
 env.render()
-print(type(obs))
-print(obs.shape)
-get_major_color(obs, Rect(0, 0, 2, 2))
+img_h, img_w, nb_channels = obs.shape
+area = Rect(638, 478, 4, 4)
+reshape_area(img_w, img_h, area)
+print(get_major_color(obs, area))
 img = Image.fromarray(obs)
 img.save("test.png")
 
