afa9b404e5315fd2754575a98ed431a4eed67645
Pr0m3th3usEx <thomas1.michel@epitech.eu>
2020-04-30 23:24:09 +0200

    [FIX] color.py : functions gathered into class ImageZone

--- color.py
+++ color.py
@@ -12,55 +12,56 @@ from pyglet.window import key
 from PIL import Image
 
 
-class Rect:
+class ImageZone:
     def __init__(self, x, y, w, h):
         self.x = x
         self.y = y
         self.w = w
         self.h = h
+
+    """
+        reshape : Resize the rect to fit the size of the image
+
+        Arguments : img_w, img_h
+        Return : self
+    """
+    def reshape(self, img_w, img_h):
+        if self.x > img_w or self.y > img_h:
+            raise ValueError
+        if self.x + self.w > img_w:
+            self.w = self.x + self.w - img_w
+        if self.y + self.h > img_h:
+            self.h = self.y + self.h - img_h
+        print(self)
+        return self
+
+    """
+        get_major_color : From a Pixel array, return the dominant color of a specific area
+
+        Arguments : img_array : np.array, area: ImageZone
+        Return : (R, G, B): tuple , covered_area: float
+    """
+    def get_major_color(self, img_array: np.ndarray):
+        dom_r, dom_g, dom_b = 0, 0, 0
+        div = self.w * self.h
+
+        for off_y in range(self.w):
+            for off_x in range(self.h):
+                pixel = img_array[self.y + off_y, self.x + off_x]
+                dom_r += pixel[0]
+                dom_g += pixel[1]
+                dom_b += pixel[2]
+        return dom_r / div, dom_g / div, dom_b / div
+
     def __str__(self):
-        return f"Rect: x: {self.x}, y: {self.y}, w: {self.w}, h: {self.h}"
+        return f"ImageZone: x: {self.x}, y: {self.y}, w: {self.w}, h: {self.h}"
+
 
 env = gym.make("Duckietown-udem1-v0")
 
 env.reset()
 env.render()
 
-"""
-    reshape_area : Resize the rect to fit the size of the image
-    
-    Arguments : img_w, img_h : size of the image, Rect()
-    Return : Rect
-"""
-def reshape_area(img_w, img_h, area):
-    if area.x > img_w or area.y > img_h:
-        raise ValueError
-    if area.x + area.w > img_w:
-        area.w = area.x + area.w - img_w
-    if area.y + area.h > img_h:
-        area.h = area.y + area.h - img_h
-    print(area)
-    return area
-
-
-"""
-    get_major_color : From a Pixel array, return the dominant color of a specific area
-    
-    Arguments : img_array : np.array, area: Rect
-    Return : (R, G, B): tuple , covered_area: float
-"""
-def get_major_color(img_array:np.ndarray, area):
-    max_w, max_h, nb_channels = img_array.shape
-    dom_r, dom_g, dom_b = 0, 0, 0
-    div = area.w * area.h
-
-    for off_y in range(area.w):
-        for off_x in range(area.h):
-            pixel = img_array[area.y + off_y, area.x + off_x]
-            dom_r += pixel[0]
-            dom_g += pixel[1]
-            dom_b += pixel[2]
-    return dom_r / div, dom_g / div, dom_b / div
 
 @env.unwrapped.window.event
 def on_key_press(symbol, modifier):
@@ -75,9 +76,9 @@ action = np.array([0.3, 0.00])
 obs, reward, done, info = env.step(action)
 env.render()
 img_h, img_w, nb_channels = obs.shape
-area = Rect(638, 478, 4, 4)
-reshape_area(img_w, img_h, area)
-print(get_major_color(obs, area))
+area = ImageZone(638, 478, 4, 4)
+area.reshape(img_w, img_h)
+print(area.get_major_color(obs))
 img = Image.fromarray(obs)
 img.save("test.png")
 
