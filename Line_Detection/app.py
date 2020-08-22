896f8b26d5e555f66f33fc951d5c5cd9babc1cb1
Pr0m3th3uEx <thomas1.michel@epitech.eu>
2020-08-22 14:10:46 +0200

    Add Sign Deteciton Visualizer into app.py (Cam + img)

--- Line_Detection/app.py
+++ Line_Detection/app.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python3
 
 import argparse
+import sys
 
 class App:
 
@@ -10,19 +11,31 @@ class App:
     def config_parser(self):
         self.parser.add_argument("--video-name", default=None)
         self.parser.add_argument("--duckietown", default=False, action="store_true")
+        sign_detection_subparser = self.parser.add_subparsers()
+        parser_sign = sign_detection_subparser.add_parser('sign_detection')
+        parser_sign.add_argument('--cam', action="store_true", default=None)
+        parser_sign.add_argument("--img", default=None)
         return self
 
     def parse(self):
         args = self.parser.parse_args()
-        if args.duckietown:
-            from visualizer.controlled import ControlledVisualizer
-            vis = ControlledVisualizer("Duckietown-udem1-v0")
-        elif args.video_name is not None:
-            from visualizer.video import VideoVisualizer
-            vis = VideoVisualizer(args.video_name)
+        if sys.argv[1] == "sign_detection":
+            from visualizer.SignVisualizer import SignVisualizer
+            print(args)
+            if args.cam:
+                vis = SignVisualizer(target="cam")
+            if args.img is not None:
+                vis = SignVisualizer(target=args.img)
         else:
-            self.help()
-            raise ValueError("No valid arguments passed")
+            if args.duckietown:
+                from visualizer.controlled import ControlledVisualizer
+                vis = ControlledVisualizer("Duckietown-udem1-v0")
+            elif args.video_name is not None:
+                from visualizer.video import VideoVisualizer
+                vis = VideoVisualizer(args.video_name)
+            else:
+                self.help()
+                raise ValueError("No valid arguments passed")
         vis.run()
 
     @staticmethod
