83693d0b8244597a3ed502dd5e78a5ca78112210
Pr0m3th3uEx <thomas1.michel@epitech.eu>
2020-09-03 00:09:33 +0200

    Update dataset.py preparing dataset images to train Network

--- RCNN/training/dataset.py
+++ RCNN/training/dataset.py
@@ -2,13 +2,21 @@
 
 import os
 from pyimagesearch import config
+from pyimagesearch.iou import compute_iou
 from imutils import paths
 from bs4 import BeautifulSoup
 import cv2
 
 class DatasetBuilder:
     def __init__(self):
-        pass
+        self.counters = [{"label": label, "counter": 0} for label in config.LABELS]
+        print (self.counters)
+
+    def get_counter_index(self, label):
+        for i in range(len(self.counters)):
+            if self.counters[i]["label"] == label:
+                return i
+        return -1
 
     def parseXMLAnnotations(self, annothPath):
         contents = open(annothPath).read()
@@ -25,11 +33,31 @@ class DatasetBuilder:
             xMax = min(w, int(o.find("xmax").string))
             yMax = min(h, int(o.find("ymax").string))
 
-            gtBoxes.append((xMin, yMin, xMax, yMax)
+            gtBoxes.append((label, xMin, yMin, xMax, yMax))
         return gtBoxes
 
+    def runSS(self, image):
+        ss = cv2.ximgproc.segmentation.createSelectiveSearchSegmentation()
+        ss.setBaseImage(image)
+        ss.switchToSelectiveSearchFast()
+        rects = ss.process()
+
+        proposedRect = []
+        for (x, y, w, h) in rects:
+            proposedRect.append((x, y, x + w, y + h))
+        return proposedRect
+
+    def isFullOverlap(self, gtBox, proposedRect):
+        (propStartX, propStartY, propEndX, propEndY) = proposedRect
+        (label, gtStartX, gtStartY, gtEndX, gtEndY) = gtBox
+
+        fullOverlap = propStartX >= gtStartX
+        fullOverlap = fullOverlap and propStartY >= gtStartY
+        fullOverlap = fullOverlap and propEndX <= gtEndX
+        fullOverlap = fullOverlap and propEndY <= gtEndY
+        return fullOverlap
+
     def processImages(self):
-        totals = [0] * len(config.LABELS_PATH)
         imagePaths = list(paths.list_images(config.ORIG_IMAGES))
         for (i, imagePath) in enumerate(imagePaths):
             print("[+] processing image {}/{}...".format(i + 1, len(imagePaths)))
@@ -39,14 +67,34 @@ class DatasetBuilder:
             gtBoxes = self.parseXMLAnnotations(annotPath)
 
             image = cv2.imread(imagePath)
-            ss = cv2.ximgproc.segmentation.createSelectiveSearchSegmentation()
-            ss.setBaseImage(image)
-            ss.switchToSelectiveSearchFast()
-            rects = ss.process()
+            proposedRects = self.runSS(image)
+            positiveROIs = 0
+            negativeROIs = 0
+            for proposedRect in proposedRects:
+                (propStartX, propStartY, propEndX, propEndY) = proposedRect
+                for gtBox in gtBoxes:
+                    iou = compute_iou(gtBox[1:], proposedRect)
+                    (label, gtStartX, gtStartY, gtEndX, gtEndY) = gtBox
 
-            proposedRect = []
-            for (x, y, w, h) in rects:
-                proposedRect.append((x, y, x + w, y + h))
+                    roi = None
+                    outputPath = None
+
+                    if iou > 0.7 and positiveROIs <= config.MAX_POSITIVE:
+                        roi = image[propStartY:propEndY, propStartX:propEndX]
+                        filename = f"{self.counters[self.get_counter_index(label)]['counter']}.png"
+                        outputPath = os.path.sep.join([config.LABELS_PATH[self.get_counter_index(label)], filename])
+                        positiveROIs += 1
+                        self.counters[self.get_counter_index(label)]['counter'] += 1
+
+                    if not self.isFullOverlap(gtBox, proposedRect) and iou < 0.05 and negativeROIs <= config.MAX_NEGATIVE:
+                        roi = image[propStartY:propEndY, propStartX:propEndX]
+                        filename = f"{self.counters[self.get_counter_index('nothing')]['counter']}.png"
+                        outputPath = os.path.sep.join([config.LABELS_PATH[self.get_counter_index(label)], filename])
+                        negativeROIs += 1
+
+                    if roi is not None and outputPath is not None:
+                        roi = cv2.resize(roi, config.INPUT_DIMS, interpolation=cv2.INTER_CUBIC)
+                        cv2.imwrite(outputPath, roi)
 
     def prepare_dataset(self):
         for _dir in config.LABELS_PATH:
--- RCNN/training/pyimagesearch/config.py
+++ RCNN/training/pyimagesearch/config.py
@@ -5,7 +5,7 @@ ORIG_IMAGES = os.path.sep.join([ORIG_BASE_PATH, "images"])
 ORIG_ANNOTS = os.path.sep.join([ORIG_BASE_PATH, "annotations"])
 
 BASE_PATH = "dataset"
-LABELS = ["stop", "nothing"]
+LABELS = ["stop_sign", "nothing"]
 LABELS_PATH = [os.path.sep.join([BASE_PATH, label]) for label in LABELS]
 
 MAX_PROPOSALS = 2000
@@ -14,7 +14,7 @@ MAX_PROPOSALS_INFER = 200
 MAX_POSITIVE = 30
 MAX_NEGATIVE = 10
 
-INPUTS_DIMS = (224,224)
+INPUT_DIMS = (224,224)
 
 MODEL_PATH = "sing_detector.h5"
 ENCODER_PATH = "label_encoder.pickle"
